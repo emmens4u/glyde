45ef59f13df962c20ee8cadc0b57cbdf54800fad
Emmanuel Mensah <emomensa@gmail.com>
2025-04-14 16:46:31 +0000

    feat: dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal, Signal } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { User } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,6 +13,7 @@ export class DashboardComponent {
   user: Signal<null | User> = signal(null);
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
+  private readonly toastr = inject(ToastrService);
 
   constructor() {
     this.user = this.authService.getCurrentuser();
@@ -21,9 +23,9 @@ export class DashboardComponent {
     try {
       await this.authService.logout();
       this.router.navigateByUrl('/login');
-      console.log('User logged out successfully');
-    } catch (error) {
-      console.error('Error during logout:', error);
+      this.toastr.success('User logged out successfully');
+    } catch (error: string | any) {
+      this.toastr.error('Error logging out', 'Please try again later', error);
     }
   }
 }
