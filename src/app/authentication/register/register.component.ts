634aace148375e8d292a9d0e37e048738c06e14f
Emmanuel Mensah <emomensa@gmail.com>
2025-04-14 16:45:03 +0000

    feat: update registerf

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,34 +1,55 @@
 import { Component, inject } from '@angular/core';
 import { LayoutComponent } from '../layout/layout.component';
-import { FormsModule, NgForm } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
+import { passwordMatchValidator } from '../Validators/passwordMatchValidator';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [LayoutComponent, FormsModule, RouterLink],
+  imports: [
+    LayoutComponent,
+    FormsModule,
+    RouterLink,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
-  fullname = '';
-  email = '';
-  password = '';
-  confirmPassword = '';
-
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
+  private readonly toastr = inject(ToastrService);
+  private readonly fb = inject(FormBuilder);
 
-   async onSubmit(registerForm: NgForm) {
-    if (registerForm.valid) {
-      console.log('Form submitted:', registerForm.value);
-      const { email, password } = registerForm.value;
-      try {
-        await this.authService.register(email, password);
-        console.log('User registered successfully');
-        this.router.navigateByUrl('/login');
-      } catch (error) {
-        console.error('Error during registration:', error);
+  registerForm = this.fb.group({
+    fullName: [''],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required,]],
+  }, {
+    validators: passwordMatchValidator,});
+
+  async onSubmit() {
+    if (this.registerForm.valid) {
+      const { email, password } = this.registerForm.value;
+      if (email && password) {
+        try {
+          await this.authService.register(email, password);
+          this.toastr.success('Registration Successful', 'Welcome!');
+          this.router.navigateByUrl('/login');
+        } catch (error: string | any) {
+          this.toastr.error('Registration Failed', error);
+        }
       }
     }
   }
