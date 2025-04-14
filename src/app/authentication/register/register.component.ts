21350b6a9c2ac238d5c42df1c301f64e570b661f
Emmanuel Mensah <emomensa@gmail.com>
2025-04-14 10:48:51 +0000

    feat: register component

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LayoutComponent } from '../layout/layout.component';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [LayoutComponent, FormsModule, RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  fullname = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
 
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+   async onSubmit(registerForm: NgForm) {
+    if (registerForm.valid) {
+      console.log('Form submitted:', registerForm.value);
+      const { email, password } = registerForm.value;
+      try {
+        await this.authService.register(email, password);
+        console.log('User registered successfully');
+        this.router.navigateByUrl('/login');
+      } catch (error) {
+        console.error('Error during registration:', error);
+      }
+    }
+  }
 }
